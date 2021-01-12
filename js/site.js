24ba34673c103363604716cb8c1bd3ff263c3e41
Aaron <10297223+aarondevon@users.noreply.github.com>
2021-01-11 20:51:29 -0600

    Update code to check for palindrome

--- js/site.js
+++ js/site.js
@@ -1,13 +1,27 @@
+const formPalindrome = document.querySelector('#formPalindrome');
+const input = document.querySelector('#input');
 const btnCheckWord = document.querySelector('#btnCheckWord');
 const output = document.querySelector('#output');
 
-document.querySelector('#formPalindrome').addEventListener('submit', () => {
-  const input = document.getElementById('inputWord').value.replace(/[^a-z0-9]+/gi, '').toLowerCase();
-  const inputReversed = input.split('').reverse().join('');
-  console.log('original', input);
-  console.log('reversed', inputWorinputReverseddReversed);
-  outNumber.innerText = `This is the number: ${inputNumber}`; 
-  const palendrome = input === inputReversed ? 'Yup.' : 'Nope.'
-  outWord.innerText = `Is this word a palendrome? ${palendrome}`;
+
+formPalindrome.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  if (input.value.trim() === '')
+  {
+    output.innerHTML = `<span class="error">You must enter a word or phrase.</span>`;
+    formPalindrome.reset();
+  } else {
+    const inputParsed = input.value.replace(/[^a-z0-9]+/gi, '')
+    .toLowerCase();
+const inputReversed = inputParsed.split('').reverse().join('');
+const palindrome = inputParsed === inputReversed ? 'Yup.' : 'Nope.'
+
+console.log('input parsed', inputParsed);
+console.log('reversed', inputReversed);
+output.innerText = `Is ${input.value} a palindrome? ${palindrome}`;
+formPalindrome.reset();
+  }
+
 })
 
